8d2a91583468daf500f010ddc9cf7fa44920c53e
Mark <m.kranenburg@qquest.nl>
2024-05-15 13:00:41 +0200

    add header

---
+++ react-project/src/Components/Header.jsx
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">Mark Kranenburg</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
